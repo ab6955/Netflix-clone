46ffae294f305f6ec396482d6ea5411e7987483c
ab6955 <workingabd62@gmai.com>
2025-01-30 20:28:06 +0530

    Netflix UI clone

---
+++ script.js
@@ -0,0 +1,11 @@
+const cardIndex = 0; // either 0 or 1.
+const totalcards = document.querySelectorAll(".card").length; // here it is 10 .
+// console.log(totalcards);
+
+const prev_btn = document.querySelector(".prev-btn");
+prev_btn.addEventListener("click", (e) => {
+  e.target.style.backgroundColor = "red";
+});
+
+const next_btn = document.querySelector(".next-btn");
+next_btn.addEventListener("click", (e) => {});
